cf8fd65bf66e227162f8bac73f699ba42792a7b1
Mike Wu <reach@sysdevmike.com>
2019-05-24 18:21:32 +0900

    Add handler to Basic & Mailchimp init.

---
+++ REST Client/Authenticators/Mailchimp/init/handler.js
@@ -0,0 +1,20 @@
+function handler() {
+
+        var username = "anystring";
+        var password = this.props["api_key"];
+
+        this.setOutputReference("Authenticator", execRef);
+
+        function basicAuthHeaderValue() {
+            var credentials = username + ":" + password;
+            var Base64 = Java.type("java.util.Base64");
+
+            return Base64.getEncoder().encodeToString(credentials.getBytes());
+        }
+
+        function execRef() {
+            return {
+                "Authorization": "Basic " + basicAuthHeaderValue()
+            };
+        }
+}
